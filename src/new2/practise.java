82f3553b1b26a150b6474075f6ade451059cf39b
Vaishnav135 <khandekarvaishnav9@gmail.com>
2025-05-17 22:40:27 +0530

    first commit

---
+++ src/new2/practise.java
@@ -0,0 +1,21 @@
+package new2;
+
+public class practise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a =4;
+		if(a%2==0)
+{
+			
+			System.out.println("Number is Even" );
+		}
+		else 
+{
+			
+			System.out.println("Number is odd" );
+		}
+	}
+
+}
